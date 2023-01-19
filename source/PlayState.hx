9ac4c917ba81585277b75ab62e69f32539e97f0e
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-01-19 18:10:54 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -675,20 +675,20 @@ class PlayState extends FlxTransitionableState
 	private function keyShit():Void
 	{
 		// HOLDING
-		var up = Controls.UP;
-		var right = Controls.RIGHT;
-		var down = Controls.DOWN;
-		var left = Controls.LEFT;
+		static var up = Controls.UP;
+		static var right = Controls.RIGHT;
+		static var down = Controls.DOWN;
+		static var left = Controls.LEFT;
 
-		var upP = Controls.UP_P;
-		var rightP = Controls.RIGHT_P;
-		var downP = Controls.RIGHT_P;
-		var leftP = Controls.LEFT_P;
+		static var upP = Controls.UP_P;
+		static var rightP = Controls.RIGHT_P;
+		static var downP = Controls.RIGHT_P;
+		static var leftP = Controls.LEFT_P;
 
-		var upR = Controls.UP_R;
-		var rightR = Controls.RIGHT_R;
-		var downR = Controls.DOWN_R;
-		var leftR = Controls.LEFT_R;
+		static var upR = Controls.UP_R;
+		static var rightR = Controls.RIGHT_R;
+		static var downR = Controls.DOWN_R;
+		static var leftR = Controls.LEFT_R;
 
 		FlxG.watch.addQuick('asdfa', upP);
 		if ((upP || rightP || downP || leftP) && !boyfriend.stunned && generatedMusic)
