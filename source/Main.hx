097828783824ccef2cfbd8c521b37d535f08ce3e
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-01-19 21:15:17 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -8,7 +8,7 @@ class Main extends Sprite
 {
 	public function new()
 	{
-                SUtil.uncaughtErrorHandler()
+                SUtil.uncaughtErrorHandler();
 		super();
 		addChild(new FlxGame(0, 0, FreeplayState));
 		addChild(new FPS(10, 3, 0xFFFFFF));
