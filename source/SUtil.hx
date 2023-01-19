3943bf66b92551711685fb5b0f72fff62c7f152d
mcagabe19 <egzozu.be2.bas@gmail.com>
2023-01-19 21:03:33 +0300

    blah blah blah so lazy to write

---
+++ source/SUtil.hx
@@ -0,0 +1,66 @@
+package;
+
+import haxe.CallStack;
+import lime.system.System as LimeSystem;
+import openfl.Lib;
+import openfl.events.UncaughtErrorEvent;
+#if web
+import haxe.Log;
+#end
+
+using StringTools;
+
+class SUtil
+{
+	public static function uncaughtErrorHandler():Void
+	{
+		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onError);
+		Lib.application.onExit.add(function(exitCode:Int)
+		{
+			if (Lib.current.loaderInfo.uncaughtErrorEvents.hasEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR))
+				Lib.current.loaderInfo.uncaughtErrorEvents.removeEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onError);
+		});
+	}
+
+	private static function onError(e:UncaughtErrorEvent):Void
+	{
+		var stack:Array<String> = [];
+		stack.push(e.error);
+
+		for (stackItem in CallStack.exceptionStack(true))
+		{
+			switch (stackItem)
+			{
+				case CFunction:
+					stack.push('Non-Haxe (C) Function');
+				case Module(m):
+					stack.push('Module ($m)');
+				case FilePos(s, file, line, column):
+					stack.push('$file (line $line)');
+				case Method(classname, method):
+					stack.push('$classname (method $method)');
+				case LocalFunction(name):
+					stack.push('Local Function ($name)');
+			}
+		}
+
+		e.preventDefault();
+		e.stopPropagation();
+		e.stopImmediatePropagation();
+
+		final msg:String = stack.join('\n');
+
+		println(msg);
+		Lib.application.window.alert(msg, 'Error!');
+		LimeSystem.exit(1);
+	}
+
+	private static function println(msg:String):Void
+	{
+		#if sys
+		Sys.println(msg);
+		#else
+		Log.trace(msg, null); // Pass null to exclude the position.
+		#end
+	}
+}
